2ddfab94facb825844f80a4235c1eda2a7a77bfc
Kiran Basnet <basnetkiran041@gmail.com>
2025-05-04 13:38:09 +0545

    4th_may_II

---
+++ 10_ifelse_condition.php
@@ -0,0 +1,14 @@
+<?php
+$a = 78;
+$b =23;
+
+if ($a>$b) {
+
+echo "a is greater than b";
+}
+
+else{
+    echo "b is greater than a";
+}
+
+?>
---
+++ 11_switch_case.php
@@ -0,0 +1,19 @@
+<?php
+
+$marks = 40;
+
+switch($marks){
+    case 33:
+        echo "Your marks is 33 </br>";
+        break;
+    case 40:
+        echo "Your marks is 40 </br>";
+        break;
+    case 23:
+        echo "Your marks is 23 </br>";
+        break;
+    case 60:
+        echo "Your marks is 60 </br>";
+        break;
+}
+?>
